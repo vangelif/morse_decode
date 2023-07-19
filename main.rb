adedc7e1adbe248e8c55e6d6674f951a198c7463
vangelif <ev.fotiades@yahoo.com>
2023-07-19 16:50:48 +0300

    fix linters

--- main.rb
+++ main.rb
@@ -1,42 +1,43 @@
 MORSE_CODE = {
-  '.-'    => 'A', 
-  '-...'  => 'B',
-  '-.-.'  => 'C',
-  '-..'   => 'D',
-  '.'     => 'E',
-  '..-.'  => 'F',
-  '--.'   => 'G',
-  '....'  => 'H',
-  '..'    => 'I',
-  '.---'  => 'J',
-  '-.-'   => 'K',
-  '.-..'  => 'L',
-  '--'    => 'M',
-  '-.'    => 'N',
-  '---'   => 'O',
-  '.--.'  => 'P',
-  '--.-'  => 'Q',
-  '.-.'   => 'R',
-  '...'   => 'S',
-  '-'     => 'T',
-  '..-'   => 'U',
-  '...-'  => 'V',
-  '.--'   => 'W',
-  '-..-'  => 'X',
-  '-.--'  => 'Y',
-  '--..'  => 'Z',
-}
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 def decode_char(morse_char)
-    MORSE_CODE[morse_char] || ''
-  end
-  def decode_word(morse_word)
-    morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join('')
-  end
-  def decode(message)
-    words = message.split('   ')
-    words.map { |morse_word| decode_word(morse_word) }.join(' ')
-  end
+  MORSE_CODE[morse_char] || ''
+end
 
-  message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
-  puts decode(message)
+def decode_word(morse_word)
+  morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+end
 
+def decode(message)
+  words = message.split('   ')
+  words.map { |morse_word| decode_word(morse_word) }.join(' ')
+end
+
+message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+puts decode(message)
